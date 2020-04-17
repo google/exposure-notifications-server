c6ac9aaa64ffab25709f79fbf38029ad89ddfab0
Guray Alsac <guray@google.com>
2020-04-17 13:41:09 -0700

    initialize decryption
    Change-Id: Ibaee5d81c2fe50d026335bc6bfa74ab1cbcc914e

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -7,6 +7,7 @@ import (
 
 	"cambio/pkg/api"
 	"cambio/pkg/database"
+	"cambio/pkg/encryption"
 	"cambio/pkg/logging"
 
 	"github.com/gorilla/mux"
@@ -19,6 +20,9 @@ func main() {
 	if err := database.Initialize(); err != nil {
 		logger.Fatalf("unable to connect to database: %v", err)
 	}
+        if err := encryption.InitDiagnosisKeys(); err != nil {
+                logger.Fatalf("encryption.InitDiagnosisKeys: %v", err)
+        }
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", api.HandleGenerateBatch())
