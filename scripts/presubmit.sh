323ed7833d9745c67ca14843d2067c1877d23b5d
Mike Helmick <helmick@google.com>
2020-04-20 18:15:44 -0400

    make presubmit script more robust
    Change-Id: I39f409cc97aecea407f31a18cc01000a3a417f82

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -1,14 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -o pipefail
+set -eEuo pipefail
 
 source_dirs="cmd pkg"
 
 echo "🚒 Update Protobufs"
 protoc --proto_path=. --go_out=plugins=grpc:. ./pkg/pb/*.proto
 
-echo "🧽 Cleanup Imports"
-goimports -w $(echo $source_dirs)
+set +e
+which goimports >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+   echo "✋ No 'goimports' found. Please use"
+   echo "✋   go install golang.org/x/tools/cmd/goimports"
+   echo "✋ to enable import cleanup. Import cleanup skipped."
+else
+   echo "🧽 Format"
+   goimports -w $(echo $source_dirs)
+fi
+set -e
 
 echo "🧹 Format Go code"
 find $(echo $source_dirs) -name "*.go" -print0 | xargs -0 gofmt -s -w
@@ -20,7 +29,5 @@ go mod tidy
 echo "🚧 Compile"
 go build ./...
 
-echo "🧪 ${X}Test"
+echo "🧪 Test"
 go test ./...
-
-
