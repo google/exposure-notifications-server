5168109911d51c31a8141ab84757ee573911af16
Guray Alsac <guray@google.com>
2020-04-30 17:59:43 -0700

    use compact byte array representation
    This is a "columnar" representation of exposure keys
    taking advantage of fixed width bytes in arrays with
    matching indexes. It offers a good tradeoff in efficiency
    of parsing while also providing full flexibility and
    extensibility.
    
    Still TBD is the signature and signing key information.

--- internal/api/export_file.go
+++ internal/api/export_file.go
@@ -17,6 +17,7 @@ package api
 
 import (
 	"bytes"
+	"encoding/binary"
 	"sort"
 	"time"
 
@@ -27,18 +28,8 @@ import (
 )
 
 func MarshalExportFile(since, until time.Time, exposureKeys []*model.Infection, region string) ([]byte, error) {
-	contents, err := marshalContents(since, until, exposureKeys, region)
-	if err != nil {
-		return nil, err
-	}
-	sig, err := sign(contents)
-	if err != nil {
-		return nil, err
-	}
-	return append(sig, contents...), nil
-}
-
-func marshalContents(since, until time.Time, exposureKeys []*model.Infection, region string) ([]byte, error) {
+	// start with fixed width 16 byte header of basic file info and version
+	exportBytes := []byte("EK Export v1   ")
 	// We want a deterministic ordering so signatures can be generated/verified consistently.
 	// Arbitrarily sorting on the keys themselves.
 	// This could be done at the db layer but doing it here makes it explicit that its
@@ -46,25 +37,48 @@ func marshalContents(since, until time.Time, exposureKeys []*model.Infection, re
 	sort.Slice(exposureKeys, func(i, j int) bool {
 		return bytes.Compare(exposureKeys[i].ExposureKey, exposureKeys[j].ExposureKey) < 0
 	})
-	var pbeks []*pb.ExposureKeyExport_ExposureKey
+	// Build up the various compact byte arrays from exposure keys
+	var keys []byte
+	var risks []byte
+	var intervals []byte
+	var rollings []byte
 	for _, ek := range exposureKeys {
-		pbek := pb.ExposureKeyExport_ExposureKey{
-			ExposureKey:    ek.ExposureKey,
-			IntervalNumber: ek.IntervalNumber,
-			IntervalCount:  ek.IntervalCount,
+		keys = append(keys, ek.ExposureKey...)
+		risks = append(risks, byte(ek.TransmissionRisk))
+
+		interval, err := convert(ek.IntervalNumber)
+		if err != nil {
+			return nil, err
 		}
-		pbeks = append(pbeks, &pbek)
+		intervals = append(intervals, interval...)
+
+		rolling, err := convert(ek.IntervalCount)
+		if err != nil {
+			return nil, err
+		}
+		rollings = append(rollings, rolling...)
 	}
-	batch := pb.ExposureKeyExport{
-		StartTimestamp: since.Unix(),
-		EndTimestamp:   until.Unix(),
-		Region:         region,
-		Keys:           pbeks,
+	m := pb.ExposureKeyExport{
+		StartTimestamp:    proto.Uint64(uint64(time.Now().Unix())),
+		EndTimestamp:      proto.Uint64(uint64(time.Now().Add(time.Hour * 24 * -1).Unix())),
+		Region:            proto.String("US"),
+		ExposureKeys:      keys,
+		TransmissionRisks: risks,
+		IntervalNumbers:   intervals,
+		RollingPeriods:    rollings,
 	}
-	return proto.Marshal(&batch)
+	pBbytes, err := proto.Marshal(&m)
+	if err != nil {
+		panic("unable to marshal protobuf")
+	}
+	return append(exportBytes, pBbytes...), nil
 }
 
-func sign(contents []byte) ([]byte, error) {
-	// TODO(guray): generate signature
-	return []byte{}, nil
+func convert(i int32) ([]byte, error) {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.LittleEndian, uint16(i))
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
 }
