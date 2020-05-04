05502f7f2d1dbfa47d0f4cd3136178d30bc7bf5d
Jonathan Amsterdam <jba@users.noreply.github.com>
2020-05-04 15:42:22 -0400

    internal/api/config: remove error return (#53)
    Remove the unused return value from loadConfig.

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -77,10 +77,6 @@ func (c *Config) loadConfig(ctx context.Context) error {
 
 	configs, err := c.db.ReadAPIConfigs(ctx)
 	if err != nil {
-		// This will exit the server. Without a valid config, we cannot process
-		// requests.
-		// TODO(mikehelmick) stable fallbacks
-		logger.Fatalf("error loading APIConfig: %v", err)
 		return err
 	}
 
@@ -90,12 +86,13 @@ func (c *Config) loadConfig(ctx context.Context) error {
 	}
 	logger.Info("loaded new APIConfig values")
 	c.lastLoadTime = time.Now()
-
 	return nil
 }
 
-func (c *Config) AppPkgConfig(ctx context.Context, appPkg string) *model.APIConfig {
-	c.loadConfig(ctx)
+func (c *Config) AppPkgConfig(ctx context.Context, appPkg string) (*model.APIConfig, error) {
+	if err := c.loadConfig(ctx); err != nil {
+		return nil, err
+	}
 
 	c.mu.RLock()
 	defer c.mu.RUnlock()
@@ -104,7 +101,7 @@ func (c *Config) AppPkgConfig(ctx context.Context, appPkg string) *model.APIConf
 	if !ok {
 		logger := logging.FromContext(ctx)
 		logger.Errorf("requested config for unconfigured app: %v", appPkg)
-		return nil
+		return nil, nil
 	}
-	return cfg
+	return cfg, nil
 }
