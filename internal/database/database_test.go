c70acbc452ff539a40ab301374ba2fac13a78535
Jonathan Amsterdam <jba@google.com>
2020-05-06 07:00:51 -0400

    internal/database: add test for DB time behavior
    Add a test that demonstrates why TIMESTAMP is a bad choice for DB type
    when a true timestamp (point in time) is desired. It does not preserve
    times.

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -20,9 +20,17 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/google/exposure-notifications-server/internal/serverenv"
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	// imported to register the postgres migration driver
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	// imported to register the "file" source migration driver
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	// imported to register the "postgres" database driver for migrate
 )
 
 var testDB *DB
@@ -102,4 +110,54 @@ func (db *DB) createDatabase(ctx context.Context, name string) error {
 	}
 	_, err = conn.Exec(ctx, fmt.Sprintf(`CREATE DATABASE %q`, name))
 	return err
+}
+
+func TestTimestampTypes(t *testing.T) {
+	// Demonstrate that the TIMESTAMP type does not preserve non-UTC times, but
+	// the TIMESTAMPTZ type does.
+	//
+	// The reason is that TIMESTAMP stores only the calendar time values (year,
+	// month, day, hour, minute, second, microsecond), while TIMESTAMPTZ also
+	// records the timezone, giving a location-independent point in time.
+	if testDB == nil {
+		t.Skip("no test DB")
+	}
+	ctx := context.Background()
+	conn, err := testDB.pool.Acquire(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Release()
+
+	mustExec(t, conn, `CREATE TABLE timestamps (no_tz TIMESTAMP, tz TIMESTAMPTZ)`)
+
+	local := time.Now()                      // timezone is always the local one, not UTC
+	local = local.Truncate(time.Microsecond) // Postgres time resolution is microseconds.
+	if name, _ := local.Zone(); name == "UTC" {
+		t.Fatalf("time.Now returned %s, which is UTC", local)
+	}
+	// Insert the same time into the DB as both a TIMESTAMP and a TIMESTAMPTZ (aka
+	// TIMESTAMP WITH TIME ZONE).
+	mustExec(t, conn, `INSERT INTO timestamps (no_tz, tz) VALUES ($1, $2)`, local, local)
+	// Read the times back.
+	var gotNoTZ, gotWithTZ time.Time
+	if err := conn.QueryRow(ctx, `SELECT no_tz, tz FROM timestamps`).Scan(&gotNoTZ, &gotWithTZ); err != nil {
+		t.Fatal(err)
+	}
+	// The TIMESTAMPTZ column is the same time.
+	if !local.Equal(gotWithTZ) {
+		t.Fatal("TIMESTAMPTZ is not the same time")
+	}
+
+	// The TIMESTAMP column is NOT the same time.
+	if local.Equal(gotNoTZ) {
+		t.Fatal("TIMESTAMP is the same time")
+	}
+}
+
+func mustExec(t *testing.T, conn *pgxpool.Conn, stmt string, args ...interface{}) {
+	_, err := conn.Exec(context.Background(), stmt, args...)
+	if err != nil {
+		t.Fatalf("executing %s: %v", stmt, err)
+	}
 }
