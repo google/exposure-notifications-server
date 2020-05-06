8aaeb9da3b1837661edb68bc198771ab16a88d23
Mike Helmick <helmick@google.com>
2020-05-06 10:17:16 -0700

    add metrics to publish API

--- internal/api/publish/publish.go
+++ internal/api/publish/publish.go
@@ -25,6 +25,7 @@ import (
 	"github.com/google/exposure-notifications-server/internal/database"
 	"github.com/google/exposure-notifications-server/internal/logging"
 	"github.com/google/exposure-notifications-server/internal/model"
+	"github.com/google/exposure-notifications-server/internal/serverenv"
 	"github.com/google/exposure-notifications-server/internal/verification"
 )
 
@@ -34,16 +35,18 @@ const (
 )
 
 // NewHandler creates the HTTP handler for the TTK publishing API.
-func NewHandler(ctx context.Context, db *database.DB, cfg *config.Config) http.Handler {
+func NewHandler(ctx context.Context, db *database.DB, cfg *config.Config, env *serverenv.ServerEnv) http.Handler {
 	return &publishHandler{
 		config: cfg,
 		db:     db,
+		env:    env,
 	}
 }
 
 type publishHandler struct {
 	config *config.Config
 	db     *database.DB
+	env    *serverenv.ServerEnv
 }
 
 // There is a target normalized latency for this function. This is to help prevent
@@ -51,12 +54,14 @@ type publishHandler struct {
 func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	logger := logging.FromContext(ctx)
+	metrics := h.env.MetricsExporter(ctx)
 
 	var data model.Publish
 	code, err := jsonutil.Unmarshal(w, r, &data)
 	if err != nil {
 		// Log the unparsable JSON, but return success to the client.
 		logger.Errorf("error unmarhsaling API call, code: %v: %v", code, err)
+		metrics.WriteInt("publish-bad-json", false, 1)
 		w.WriteHeader(http.StatusOK)
 		return
 	}
@@ -66,12 +71,14 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// Log the configuraiton error, return error to client.
 		// This is retryable, although won't succede if the error isn't transient.
 		logger.Errorf("no API config, dropping data: %v", err)
+		metrics.WriteInt("publish-error-loading-apiconfig", false, 1)
 		http.Error(w, "internal error", http.StatusInternalServerError)
 		return
 	}
 	if cfg == nil {
 		// configs were loaded, but the request app isn't configured.
 		logger.Errorf("unauthorized application: %v", data.AppPackageName)
+		metrics.WriteInt("publish-app-not-authorized", false, 1)
 		// This returns success to the client.
 		w.WriteHeader(http.StatusOK)
 		return
@@ -80,6 +87,7 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err = verification.VerifyRegions(cfg, data)
 	if err != nil {
 		logger.Errorf("verification.VerifyRegions: %v", err)
+		metrics.WriteInt("publish-region-not-authorized", false, 1)
 		// This returns success to the client.
 		w.WriteHeader(http.StatusOK)
 		return
@@ -87,6 +95,7 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if cfg.IsIOS() {
 		logger.Errorf("ios devicecheck not supported on this server.")
+		metrics.WriteInt("publish-no-ios", false, 1)
 		// This returns success to the client.
 		w.WriteHeader(http.StatusOK)
 		return
@@ -94,12 +103,14 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		err = verification.VerifySafetyNet(ctx, time.Now().UTC(), cfg, data)
 		if err != nil {
 			logger.Errorf("unable to verify safetynet payload: %v", err)
+			metrics.WriteInt("publish-safetnet-invalid", false, 1)
 			// This returns success to the client.
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 	} else {
 		logger.Errorf("invalid API configuration for AppPkg: %v, invalid platform", data.AppPackageName)
+		metrics.WriteInt("publish-apiconfig-missing-platform", false, 1)
 		// This returns success to the client.
 		w.WriteHeader(http.StatusOK)
 		return
@@ -109,6 +120,7 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	exposures, err := model.TransformPublish(&data, batchTime)
 	if err != nil {
 		logger.Errorf("error transforming publish data: %v", err)
+		metrics.WriteInt("publish-transform-fail", false, 1)
 		// This returns success to the client.
 		w.WriteHeader(http.StatusOK)
 		return
@@ -117,10 +129,12 @@ func (h *publishHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err = h.db.InsertExposures(ctx, exposures)
 	if err != nil {
 		logger.Errorf("error writing exposure record: %v", err)
+		metrics.WriteInt("publish-db-write-error", false, 1)
 		// This is retryable at the client - database error at the server.
 		http.Error(w, "internal processing error", http.StatusInternalServerError)
 		return
 	}
+	metrics.WriteInt("publish-exposures-written", true, len(exposures))
 	logger.Infof("Inserted %d exposures.", len(exposures))
 
 	w.WriteHeader(http.StatusOK)
