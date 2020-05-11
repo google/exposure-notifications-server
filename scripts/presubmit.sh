9ae333c41b5372160d72069c2ad0add844864a86
Chris Wilcox <crwilcox@google.com>
2020-05-11 14:17:44 -0700

    find cgroup for prow

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -103,7 +103,8 @@ go test ./... -coverprofile=coverage.out
 
 
 echo "🧪 Test DB Tests"
-
+echo "CGROUP"
+cat /proc/1/cgroup
 if ($( cat /proc/1/cgroup | grep docker > /dev/null )); then
    echo "🚒 In CI Container, start postgres process"
    export DB_USER=postgres
