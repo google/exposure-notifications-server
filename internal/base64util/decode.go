a367cdfb8ebbea8fdfadb2e7e645cee4136ccee3
Seth Vargo <seth@sethvargo.com>
2020-05-12 12:26:59 -0400

    Handle padded/non-padded and std/url-encoded base64 (#189)
    This introduces a new internal package base64util that simplifies
    decoding base64 strings. It handles padded and non-padded strings as
    well as Standard and URL encoded values.
    
    Fixes GH-160

---
+++ internal/base64util/decode.go
@@ -0,0 +1,40 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package base64util extracts base64 encoding/decoding logic into a single API
+// that is tolerant of various paddings.
+package base64util
+
+import (
+	"encoding/base64"
+	"strings"
+)
+
+// DecodeString decodes the given string as base64.
+func DecodeString(s string) ([]byte, error) {
+	s = unpad(convertToURLEncoding(s))
+	return base64.RawURLEncoding.DecodeString(s)
+}
+
+// convertToURLEncoding converts the given string to URL-encoded.
+func convertToURLEncoding(s string) string {
+	s = strings.ReplaceAll(s, "+", "-")
+	s = strings.ReplaceAll(s, "/", "_")
+	return s
+}
+
+// unpad removes any padding from the string.
+func unpad(s string) string {
+	return strings.TrimRight(s, "=")
+}
