4f577188e0e9fc170f80dbe5d4286bea65b1c32c
Seth Vargo <seth@sethvargo.com>
2020-05-19 23:22:12 -0400

    Add ability to deploy

--- terraform/service_exposure.tf
+++ terraform/service_exposure.tf
@@ -22,6 +22,16 @@ resource "google_service_account" "exposure" {
   display_name = "Exposure Notification Exposure"
 }
 
+resource "google_service_account_iam_member" "cloudbuild-deploy-exposure" {
+  service_account_id = google_service_account.exposure.id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+
+  depends_on = [
+    google_project_service.services["cloudbuild.googleapis.com"],
+  ]
+}
+
 resource "google_project_iam_member" "exposure-cloudsql" {
   project = data.google_project.project.project_id
   role    = "roles/cloudsql.client"
