15b34e5dfcb07f967c74f0f09e4fff74a445365d
Seth Vargo <seth@sethvargo.com>
2020-05-27 22:21:13 -0400

    Remove unnecessary beta providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,20 +54,18 @@ resource "google_project_service" "services" {
 }
 
 resource "google_compute_global_address" "private_ip_address" {
-  provider = google-beta
-
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
   network       = data.google_compute_network.default.self_link
 
-  depends_on = [google_project_service.services["compute.googleapis.com"]]
+  depends_on = [
+    google_project_service.services["compute.googleapis.com"],
+  ]
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  provider = google-beta
-
   network                 = data.google_compute_network.default.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
