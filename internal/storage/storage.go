15c7f1aafe24275c0011aeb89ed2e4f12ae6bff1
Seth Vargo <seth@sethvargo.com>
2020-06-01 16:28:25 -0400

    Add AWS S3 blobstore (#517)

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package storage is an interface over file/blob storage
+// Package storage is an interface over file/blob storage.
 package storage
 
 import (
