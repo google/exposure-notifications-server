923dc424444353f8c758f31da2f02088d53a9c0c
Seth Vargo <seth@sethvargo.com>
2020-06-05 18:36:06 -0400

    Add initial e2e framework

---
+++ internal/integration/integration_test.go
@@ -0,0 +1,195 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/google/exposure-notifications-server/internal/authorizedapp"
+	"github.com/google/exposure-notifications-server/internal/cleanup"
+	"github.com/google/exposure-notifications-server/internal/database"
+	"github.com/google/exposure-notifications-server/internal/export"
+	"github.com/google/exposure-notifications-server/internal/federationin"
+	"github.com/google/exposure-notifications-server/internal/handlers"
+	"github.com/google/exposure-notifications-server/internal/publish"
+	"github.com/google/exposure-notifications-server/internal/secrets"
+	"github.com/google/exposure-notifications-server/internal/server"
+	"github.com/google/exposure-notifications-server/internal/serverenv"
+	"github.com/google/exposure-notifications-server/internal/signing"
+	"github.com/google/exposure-notifications-server/internal/storage"
+)
+
+func testServer(tb testing.TB) (*serverenv.ServerEnv, *http.Client) {
+	tb.Helper()
+
+	ctx := context.Background()
+
+	aa, err := authorizedapp.NewMemoryProvider(ctx, nil)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	bs, err := storage.NewMemory(ctx)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	db := database.NewTestDatabase(tb)
+
+	km, err := signing.NewNoop(ctx)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	sm, err := secrets.NewNoop(ctx)
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	env := serverenv.New(ctx,
+		serverenv.WithAuthorizedAppProvider(aa),
+		serverenv.WithBlobStorage(bs),
+		serverenv.WithDatabase(db),
+		serverenv.WithKeyManager(km),
+		serverenv.WithSecretManager(sm),
+	)
+	// Note: don't call env.Cleanup() because the database helper closes the
+	// connection for us.
+
+	mux := http.NewServeMux()
+
+	// Cleanup export
+	cleanupExportConfig := &cleanup.Config{
+		Timeout: 10 * time.Minute,
+		TTL:     336 * time.Hour,
+	}
+
+	cleanupExportHandler, err := cleanup.NewExportHandler(cleanupExportConfig, env)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	mux.Handle("/cleanup-export", cleanupExportHandler)
+
+	// Cleanup exposure
+	cleanupExposureConfig := &cleanup.Config{
+		Timeout: 10 * time.Minute,
+		TTL:     336 * time.Hour,
+	}
+
+	cleanupExposureHandler, err := cleanup.NewExposureHandler(cleanupExposureConfig, env)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	mux.Handle("/cleanup-exposure", cleanupExposureHandler)
+
+	// Export
+	exportConfig := &export.Config{
+		CreateTimeout:  10 * time.Second,
+		WorkerTimeout:  10 * time.Second,
+		MinRecords:     1,
+		PaddingRange:   1,
+		MaxRecords:     10000,
+		TruncateWindow: 1 * time.Second,
+		MinWindowAge:   1 * time.Second,
+		TTL:            336 * time.Hour,
+	}
+
+	exportServer, err := export.NewServer(exportConfig, env)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	mux.HandleFunc("/export/create-batches", exportServer.CreateBatchesHandler)
+	mux.HandleFunc("/export/do-work", exportServer.WorkerHandler)
+
+	// Federation
+	federationInConfig := &federationin.Config{
+		Timeout:        10 * time.Minute,
+		TruncateWindow: 1 * time.Hour,
+	}
+
+	mux.Handle("/federation-in", federationin.NewHandler(env, federationInConfig))
+
+	// Federation out
+	// TODO: this is a grpc listener and requires a lot of setup.
+
+	// Publish
+	publishConfig := &publish.Config{
+		MinRequestDuration:      50 * time.Millisecond,
+		MaxKeysOnPublish:        15,
+		MaxIntervalAge:          360 * time.Hour,
+		TruncateWindow:          1 * time.Hour,
+		DebugAPIResponses:       true,
+		DebugReleaseSameDayKeys: true,
+	}
+
+	publishHandler, err := publish.NewHandler(ctx, publishConfig, env)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	mux.HandleFunc("/publish", handlers.WithMinimumLatency(publishConfig.MinRequestDuration, publishHandler))
+
+	srv, err := server.New("")
+	if err != nil {
+		tb.Fatal(err)
+	}
+
+	// Stop the server on cleanup
+	stopCtx, stop := context.WithCancel(ctx)
+	tb.Cleanup(stop)
+
+	// Start the server
+	go func() {
+		if err := srv.ServeHTTPHandler(stopCtx, mux); err != nil {
+			tb.Error(err)
+		}
+	}()
+
+	// Create a client
+	client := testClient(tb, srv)
+
+	return env, client
+}
+
+type prefixRoundTripper struct {
+	addr string
+	rt   http.RoundTripper
+}
+
+func (p *prefixRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	u := r.URL
+	if u.Scheme == "" {
+		u.Scheme = "http"
+	}
+	if u.Host == "" {
+		u.Host = p.addr
+	}
+
+	return p.rt.RoundTrip(r)
+}
+
+func testClient(tb testing.TB, srv *server.Server) *http.Client {
+	prt := &prefixRoundTripper{
+		addr: srv.Addr(),
+		rt:   http.DefaultTransport,
+	}
+
+	return &http.Client{
+		Timeout:   5 * time.Second,
+		Transport: prt,
+	}
+}
---
+++ internal/integration/publish_test.go
@@ -0,0 +1,141 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	authorizedappmodel "github.com/google/exposure-notifications-server/internal/authorizedapp/model"
+	exportdatabase "github.com/google/exposure-notifications-server/internal/export/database"
+	exportmodel "github.com/google/exposure-notifications-server/internal/export/model"
+	publishdb "github.com/google/exposure-notifications-server/internal/publish/database"
+	publishmodel "github.com/google/exposure-notifications-server/internal/publish/model"
+	"github.com/google/exposure-notifications-server/internal/util"
+	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1alpha1"
+)
+
+func TestPublish(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	env, client := testServer(t)
+	db := env.Database()
+
+	// Create an authorized app.
+	aa := env.AuthorizedAppProvider()
+	if err := aa.Add(ctx, &authorizedappmodel.AuthorizedApp{
+		AppPackageName: "com.example.app",
+		AllowedRegions: map[string]struct{}{
+			"US": {},
+		},
+		AllowedHealthAuthorityIDs: map[int64]struct{}{
+			12345: {},
+		},
+
+		// TODO: hook up verification, and disable
+		BypassHealthAuthorityVerification: true,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a signature info.
+	si := &exportmodel.SignatureInfo{
+		SigningKey:        "signer",
+		AppPackageName:    "",
+		BundleID:          "",
+		SigningKeyVersion: "v1",
+		SigningKeyID:      "US",
+	}
+	if err := exportdatabase.New(db).AddSignatureInfo(ctx, si); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create an export config.
+	ec := &exportmodel.ExportConfig{
+		BucketName:       "my-bucket",
+		Period:           1 * time.Second,
+		OutputRegion:     "US",
+		From:             time.Now().Add(-2 * time.Second),
+		Thru:             time.Now().Add(1 * time.Hour),
+		SignatureInfoIDs: []int64{si.ID},
+	}
+	if err := exportdatabase.New(db).AddExportConfig(ctx, ec); err != nil {
+		t.Fatal(err)
+	}
+
+	payload := &verifyapi.Publish{
+		Keys:           util.GenerateExposureKeys(3, -1, false),
+		Regions:        []string{"US"},
+		AppPackageName: "com.example.app",
+
+		// TODO: hook up verification
+		VerificationPayload: "TODO",
+	}
+
+	var body bytes.Buffer
+	if err := json.NewEncoder(&body).Encode(payload); err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := client.Post("/publish", "application/json", &body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Ensure we get a successful response code.
+	if got, want := resp.StatusCode, http.StatusOK; got != want {
+		t.Errorf("expected %v to be %v", got, want)
+	}
+
+	// Look up the exposures in the database.
+	criteria := publishdb.IterateExposuresCriteria{
+		OnlyLocalProvenance: false,
+	}
+
+	var exposures []*publishmodel.Exposure
+	if _, err := publishdb.New(db).IterateExposures(ctx, criteria, func(m *publishmodel.Exposure) error {
+		exposures = append(exposures, m)
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := len(exposures), 3; got != want {
+		t.Errorf("expected %v to be %v: %#v", got, want, exposures)
+	}
+
+	// Create an export.
+	resp, err = client.Get("/export/create-batches")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	resp, err = client.Get("/export/do-work")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// TODO: verify export has the correct file
+
+	// TODO: verify signature
+}
