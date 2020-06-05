cb2a521e4f491ffc202cb61c2d9c8cfccc6b02ae
Mike Helmick <helmick@google.com>
2020-06-05 13:29:50 -0700

    Factor out cache implementation to standalone package. (#557)
    * Factor out cache implementation to standalone package.
    
    * Create write thru cache implementation
    * Move database provider to it
    * First part of #555
    
    Follow on PR will bring this to other parts of the codebase that use caching.
    
    * review comments + concurrency test
    
    * fix missed err return

---
+++ internal/cache/cache_test.go
@@ -0,0 +1,179 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cache implements an inmemory cache for any interface{} object.
+package cache
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+type order struct {
+	Burgers int
+	Fries   int
+}
+
+func checkSize(t *testing.T, c *Cache, want int) {
+	t.Helper()
+
+	if got := c.Size(); got != want {
+		t.Errorf("wrong size want: %v, got: %v", want, got)
+	}
+}
+
+func TestCache(t *testing.T) {
+	duration := time.Millisecond * 500
+	cache, err := New(duration)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checkSize(t, cache, 0)
+
+	if err := cache.Set("foo", &order{2, 1}); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	time.Sleep(duration)
+	if got, hit := cache.Lookup("foo"); got != nil || hit {
+		t.Fatalf("key did not expire as expected")
+	}
+
+	if got, hit := cache.Lookup("bar"); got != nil || hit {
+		t.Fatalf("got key that was never inserted")
+	}
+
+	want := &order{42, 37}
+	if err := cache.Set("foo", want); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if got, hit := cache.Lookup("foo"); got == nil || !hit {
+		t.Fatalf("lookup failed want: %v, got %v", want, got)
+	} else {
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Fatalf("mismatch (-want, +got):\n%s", diff)
+		}
+	}
+	time.Sleep(duration * 2)
+	if got, hit := cache.Lookup("foo"); got != nil || hit {
+		t.Fatalf("expected key to expire, but still available")
+	}
+	// potential race, yeild CPU so that the purge go routine has a chance to run.
+	time.Sleep(duration)
+	checkSize(t, cache, 0)
+}
+
+func TestWriteThruCache(t *testing.T) {
+	cache, err := New(time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lookupCount := 0
+	want := &order{12, 34}
+	lookerUpper := func() (interface{}, error) {
+		lookupCount++
+		return want, nil
+	}
+
+	for i := 0; i < 2; i++ {
+		got, err := cache.WriteThruLookup("foo", lookerUpper)
+		if err != nil {
+			t.Fatalf("unexpected error on WriteThruLookup: %v", err)
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Fatalf("mismatch (-want, +got):\n%s", diff)
+		}
+	}
+
+	if lookupCount != 1 {
+		t.Fatalf("incorrect lookup count, want: 1, got: %v", lookupCount)
+	}
+}
+
+func TestWriteThruError(t *testing.T) {
+	cache, err := New(time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lookerUpper := func() (interface{}, error) {
+		return nil, fmt.Errorf("nope")
+	}
+
+	got, err := cache.WriteThruLookup("foo", lookerUpper)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+	if err.Error() != "nope" {
+		t.Errorf("incorrect error, want: `nope` got: %v", err.Error())
+	}
+	if got != nil {
+		t.Errorf("unexpecetd cached item, want: nil, got %v", got)
+	}
+}
+
+func TestInvalidDuration(t *testing.T) {
+	if _, err := New(-1 * time.Second); err == nil {
+		t.Fatal("expecterd error, got nil")
+	} else if strings.Contains(err.Error(), "duration cannot be nagative") {
+		t.Fatalf("wrong error: want: `duration cannot be negative` got: %v", err.Error())
+	}
+}
+
+func TestConcurrentReaders(t *testing.T) {
+	cache, err := New(time.Second * 5)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lookupCount := 0
+	want := &order{12, 34}
+	lookerUpper := func() (interface{}, error) {
+		lookupCount++
+		return want, nil
+	}
+
+	done := make(chan bool, 10)
+	for i := 0; i < 10; i++ {
+		ver := i
+		go func() {
+			got, err := cache.WriteThruLookup("foo", lookerUpper)
+			if err != nil {
+				t.Errorf("routine: %v got unexpected error: %v", ver, err)
+			}
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Fatalf("routine: %v mismatch (-want, +got):\n%s", ver, diff)
+			}
+			done <- true
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		select {
+		case <-done:
+			continue
+		case <-time.After(1 * time.Second):
+			t.Fatal("gorountines didn't termine fast enough")
+		}
+	}
+
+	if lookupCount != 1 {
+		t.Errorf("unexpected lookupCount, want: 1, got: %v", lookupCount)
+	}
+}
