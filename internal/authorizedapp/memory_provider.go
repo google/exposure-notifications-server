9ab8764ea5d4ab42c0cac23fb36112be1e82bb84
Seth Vargo <seth@sethvargo.com>
2020-06-05 17:28:45 -0400

    Add Add() interface to authorizedapp provider

--- internal/authorizedapp/memory_provider.go
+++ internal/authorizedapp/memory_provider.go
@@ -16,7 +16,9 @@ package authorizedapp
 
 import (
 	"context"
+	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/google/exposure-notifications-server/internal/authorizedapp/model"
 )
@@ -27,26 +29,44 @@ var _ Provider = (*MemoryProvider)(nil)
 // MemoryProvider is an Provider that stores values in-memory. It is primarily
 // used for testing.
 type MemoryProvider struct {
-	Data map[string]*model.AuthorizedApp
+	lock sync.RWMutex
+	data map[string]*model.AuthorizedApp
 }
 
 // NewMemoryProvider creates a new Provider that reads from a database.
-func NewMemoryProvider(ctx context.Context, _ *Config) (Provider, error) {
+func NewMemoryProvider(_ context.Context, _ *Config) (Provider, error) {
 	provider := &MemoryProvider{
-		Data: make(map[string]*model.AuthorizedApp),
+		data: make(map[string]*model.AuthorizedApp),
 	}
-
 	return provider, nil
 }
 
 // AppConfig returns the config for the given app package name.
-func (p *MemoryProvider) AppConfig(ctx context.Context, name string) (*model.AuthorizedApp, error) {
+func (p *MemoryProvider) AppConfig(_ context.Context, name string) (*model.AuthorizedApp, error) {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
 	// Match case-insensitivy of the database.
 	name = strings.ToLower(name)
 
-	val, ok := p.Data[name]
+	val, ok := p.data[name]
 	if !ok {
 		return nil, ErrAppNotFound
 	}
 	return val, nil
+}
+
+// Add inserts the app. It returns an error if the app already exists.
+func (p *MemoryProvider) Add(_ context.Context, app *model.AuthorizedApp) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	name := strings.ToLower(app.AppPackageName)
+
+	if _, ok := p.data[name]; ok {
+		return fmt.Errorf("%v already exists", name)
+	}
+
+	p.data[name] = app
+	return nil
 }
