8b882b344953281e1c98ca5dcb3c50b739d3ca4b
Seth Vargo <seth@sethvargo.com>
2020-07-18 12:31:50 -0400

    Move server into pkg (#742)
    * Move server into pkg
    
    * Update imports

--- cmd/exposure/main.go
+++ cmd/exposure/main.go
@@ -24,8 +24,8 @@ import (
 	"github.com/google/exposure-notifications-server/internal/logging"
 	_ "github.com/google/exposure-notifications-server/internal/observability"
 	"github.com/google/exposure-notifications-server/internal/publish"
-	"github.com/google/exposure-notifications-server/internal/server"
 	"github.com/google/exposure-notifications-server/internal/setup"
+	"github.com/google/exposure-notifications-server/pkg/server"
 )
 
 func main() {
