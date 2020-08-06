5a66e090363602560ef2b9622c2e026c70295abc
Weston Haught <whaught@google.com>
2020-08-06 08:20:36 -0700

    terraform config

---
+++ terraform/service_keyrotation.tf
@@ -0,0 +1,163 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Create and deploy the service
+#
+
+resource "google_service_account" "key-rotation" {
+  project      = data.google_project.project.project_id
+  account_id   = "en-key-rotation-sa"
+  display_name = "Exposure Notification RevisionKey Rotation"
+}
+
+resource "google_service_account_iam_member" "cloudbuild-deploy-key-rotation" {
+  service_account_id = google_service_account.key-rotation.id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+
+  depends_on = [
+    google_project_service.services["cloudbuild.googleapis.com"],
+  ]
+}
+
+resource "google_secret_manager_secret_iam_member" "key-rotation-db" {
+  for_each = toset([
+    "sslcert",
+    "sslkey",
+    "sslrootcert",
+    "password",
+  ])
+
+  secret_id = google_secret_manager_secret.db-secret[each.key].id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.key-rotation.email}"
+}
+
+resource "google_project_iam_member" "key-rotation-observability" {
+  for_each = toset([
+    "roles/cloudtrace.agent",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/stackdriver.resourceMetadata.writer",
+  ])
+
+  project = var.project
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.key-rotation.email}"
+}
+
+resource "google_cloud_run_service" "key-rotation" {
+  name     = "key-rotation"
+  location = var.cloudrun_location
+
+  autogenerate_revision_name = true
+
+  template {
+    spec {
+      service_account_name = google_service_account.key-rotation.email
+
+      containers {
+        image = "gcr.io/${data.google_project.project.project_id}/github.com/google/exposure-notifications-server/cmd/key-rotation:initial"
+
+        resources {
+          limits = {
+            cpu    = "2000m"
+            memory = "1G"
+          }
+        }
+
+        dynamic "env" {
+          for_each = merge(
+            local.common_cloudrun_env_vars,
+
+            // This MUST come last to allow overrides!
+            lookup(var.service_environment, "key_rotation", {}),
+          )
+
+          content {
+            name  = env.key
+            value = env.value
+          }
+        }
+      }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" : "1",
+        "run.googleapis.com/vpc-access-connector" : google_vpc_access_connector.connector.id
+      }
+    }
+  }
+
+  depends_on = [
+    google_project_service.services["run.googleapis.com"],
+    google_secret_manager_secret_iam_member.key-rotation-db,
+    null_resource.build,
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      template[0].metadata[0].annotations,
+      template[0].spec[0].containers[0].image,
+    ]
+  }
+}
+
+
+#
+# Create scheduler job to invoke the service on a fixed interval.
+#
+
+resource "google_service_account" "key-rotation-invoker" {
+  project      = data.google_project.project.project_id
+  account_id   = "en-key-rotation-invoker-sa"
+  display_name = "Exposure Notification Key Rotation Invoker"
+}
+
+resource "google_cloud_run_service_iam_member" "key-rotation-invoker" {
+  project  = google_cloud_run_service.key-rotation.project
+  location = google_cloud_run_service.key-rotation.location
+  service  = google_cloud_run_service.key-rotation.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.key-rotation-invoker.email}"
+}
+
+resource "google_cloud_scheduler_job" "key-rotation-worker" {
+  name             = "key-rotation-worker"
+  region           = var.cloudscheduler_location
+  schedule         = "0 0 * * *" #daily
+  time_zone        = "America/Los_Angeles"
+  attempt_deadline = "600s"
+
+  retry_config {
+    retry_count = 3
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "${google_cloud_run_service.key-rotation.status.0.url}/rotate-keys"
+    oidc_token {
+      audience              = google_cloud_run_service.key-rotation.status.0.url
+      service_account_email = google_service_account.key-rotation-invoker.email
+    }
+  }
+
+  depends_on = [
+    google_app_engine_application.app,
+    google_cloud_run_service_iam_member.key-rotation-invoker,
+    google_project_service.services["cloudscheduler.googleapis.com"],
+  ]
+}
