ec4f420830251482964be0248fa2f6beab0e78b5
Chao Dai <45011425+chaodaiG@users.noreply.github.com>
2020-09-11 07:02:16 -0700

    Run terraform smoke test with dynamic projects (#976)

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -20,11 +20,39 @@ set -eEuo pipefail
 
 ROOT="$(cd "$(dirname "$0")/.." &>/dev/null; pwd -P)"
 
-# TODO: use project pool for this
-# PROW_JOB_ID is an env var set by prow, use project for prow when it's in prow
-if [[ -z "${PROJECT_ID:-}" && -n "${PROW_JOB_ID:-}" ]]; then
-    echo "Using project for prow since"
-    export PROJECT_ID=serious-physics-287516
-fi
+function main() {
+  # PROW_JOB_ID is an env var set by prow, use project for prow when it's in prow
+  if [[ -z "${PROJECT_ID:-}" && -n "${PROW_JOB_ID:-}" ]]; then
+      PROJECT_ID="$(boskos_acquire)"
+      trap "boskosctl_wrapper release --name \"${PROJECT_ID}\" --target-state dirty >&2" EXIT
+      export PROJECT_ID
+  fi
 
-${ROOT}/scripts/terraform.sh smoke
+  ${ROOT}/scripts/terraform.sh smoke
+}
+
+
+# Functions below are prow helpers
+
+# Wrapper for running all boskos commands, similar to struct functions in Go
+# JOB_NAME is defined in prow
+function boskosctl_wrapper() {
+    boskosctl --server-url "http://boskos.test-pods.svc.cluster.local." --owner-name "${JOB_NAME}" "${@}"
+}
+
+# Acquire GCP project from Boskos, the manager of projects pool
+# Return the project name being acquired
+function boskos_acquire() {
+  local resource
+  local resource_name
+  echo "Try to acquire project from boskos" >&2
+  resource="$( boskosctl_wrapper acquire --type key-smoke-project --state free --target-state busy --timeout 10m )"
+  resource_name="$( jq .name <<<"${resource}" | tr -d \" )"
+  echo "Acquired project from boskos: ${resource_name}" >&2
+  # Send a heartbeat in the background to keep the lease while using the resource.
+  boskosctl_wrapper heartbeat --resource "${resource}" >/dev/null 2>&1 &
+  echo "${resource_name}"
+}
+
+
+main
--- terraform-e2e/state.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "serious-physics-287516-tf-state"
-  }
-}
