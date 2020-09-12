a252846514836d1b83ba132c70710d5518ab26d0
Mike Helmick <helmick@google.com>
2020-09-12 06:48:16 -0700

    add migration to revert export import (#985)

---
+++ migrations/000039_UndoImportExport.up.sql
@@ -0,0 +1,30 @@
+-- Copyright 2020 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+BEGIN;
+
+-- This is a undo of migration 38 as we changed directions.
+ALTER TABLE Exposure
+    DROP COLUMN export_import_id,
+    DROP COLUMN import_file_id,
+    DROP COLUMN revised_import_file_id;
+
+DROP TABLE ImportFile;
+DROP TYPE ImportFileStatus;
+
+DROP TABLE ImportFilePublicKey;
+
+DROP TABLE ExportImport;
+
+END;
