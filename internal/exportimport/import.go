01095d5d65268ca0bb468f93243c68ad256b6b0b
Mike Helmick <helmick@google.com>
2020-10-01 15:34:04 -0700

    lint

--- internal/exportimport/import.go
+++ internal/exportimport/import.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package import imports export files into the local database
+// Package exportimport imports export files into the local database
 package exportimport
 
 import (
