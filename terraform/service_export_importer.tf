3fcfdcc5a3342e732148d7935a16d6297bf990f5
Mike Helmick <helmick@google.com>
2020-10-05 15:07:18 -0700

    add infrastructure for export-importer (#1053)

---
+++ terraform/service_export_importer.tf
@@ -0,0 +1,193 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Create and deploy the service
+#
+
+resource "google_service_account" "export-importer" {
+  project      = data.google_project.project.project_id
+  account_id   = "en-export-importer-sa"
+  display_name = "Exposure Notification Export Importer"
+}
+
+resource "google_service_account_iam_member" "cloudbuild-deploy-export-importer" {
+  service_account_id = google_service_account.export-importer.id
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+
+  depends_on = [
+    google_project_service.services["cloudbuild.googleapis.com"],
+  ]
+}
+
+resource "google_secret_manager_secret_iam_member" "export-importer-db" {
+  for_each = toset([
+    "sslcert",
+    "sslkey",
+    "sslrootcert",
+    "password",
+  ])
+
+  secret_id = google_secret_manager_secret.db-secret[each.key].id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.export-importer.email}"
+}
+
+resource "google_project_iam_member" "export-importer-observability" {
+  for_each = toset([
+    "roles/cloudtrace.agent",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/stackdriver.resourceMetadata.writer",
+  ])
+
+  project = var.project
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.export-importer.email}"
+}
+
+resource "google_cloud_run_service" "export-importer" {
+  name     = "export-importer"
+  location = var.cloudrun_location
+
+  autogenerate_revision_name = true
+
+  template {
+    spec {
+      service_account_name = google_service_account.export-importer.email
+
+      containers {
+        image = "gcr.io/${data.google_project.project.project_id}/github.com/google/exposure-notifications-server/export-importer:initial"
+
+        resources {
+          limits = {
+            cpu    = "2000m"
+            memory = "1G"
+          }
+        }
+
+        dynamic "env" {
+          for_each = merge(
+            local.common_cloudrun_env_vars,
+
+            // This MUST come last to allow overrides!
+            lookup(var.service_environment, "export-importer", {}),
+          )
+
+          content {
+            name  = env.key
+            value = env.value
+          }
+        }
+      }
+
+      container_concurrency = 10
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" : "10",
+        "run.googleapis.com/vpc-access-connector" : google_vpc_access_connector.connector.id
+      }
+    }
+  }
+
+  depends_on = [
+    google_project_service.services["run.googleapis.com"],
+    google_secret_manager_secret_iam_member.export-importer-db,
+    null_resource.build,
+    null_resource.migrate,
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      template[0].metadata[0].annotations,
+      template[0].spec[0].containers[0].image,
+    ]
+  }
+}
+
+
+#
+# Create scheduler job to invoke the service on a fixed interval.
+#
+
+resource "google_service_account" "export-importer-invoker" {
+  project      = data.google_project.project.project_id
+  account_id   = "en-export-importer-invoker-sa"
+  display_name = "Exposure Notification Export Importer Invoker"
+}
+
+resource "google_cloud_run_service_iam_member" "export-importer-invoker" {
+  project  = google_cloud_run_service.export.project
+  location = google_cloud_run_service.export.location
+  service  = google_cloud_run_service.export.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.export-importer-invoker.email}"
+}
+
+resource "google_cloud_scheduler_job" "export-importer-worker" {
+  name             = "export-importer-worker"
+  region           = var.cloudscheduler_location
+  schedule         = "* * * * *"
+  time_zone        = "America/Los_Angeles"
+  attempt_deadline = "600s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "${google_cloud_run_service.export-importer.status.0.url}/import"
+    oidc_token {
+      audience              = google_cloud_run_service.export-importer.status.0.url
+      service_account_email = google_service_account.export-importer-invoker.email
+    }
+  }
+
+  depends_on = [
+    google_app_engine_application.app,
+    google_cloud_run_service_iam_member.export-importer-invoker,
+    google_project_service.services["cloudscheduler.googleapis.com"],
+  ]
+}
+
+resource "google_cloud_scheduler_job" "export-importer-schedule" {
+  name             = "export-importer-cschedule"
+  region           = var.cloudscheduler_location
+  schedule         = "*/5 * * * *"
+  time_zone        = "America/Los_Angeles"
+  attempt_deadline = "600s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "GET"
+    uri         = "${google_cloud_run_service.export-importer.status.0.url}/schedule"
+    oidc_token {
+      audience              = google_cloud_run_service.export-importer.status.0.url
+      service_account_email = google_service_account.export-importer-invoker.email
+    }
+  }
+
+  depends_on = [
+    google_app_engine_application.app,
+    google_cloud_run_service_iam_member.export-importer-invoker,
+    google_project_service.services["cloudscheduler.googleapis.com"],
+  ]
+}
