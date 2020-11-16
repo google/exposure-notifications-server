08fb738c785f56386d80482c08ffff766f5aa1d3
Mike Helmick <helmick@google.com>
2020-11-16 08:40:44 -0800

    change to arrays

--- migrations/000048_HealthAuthorityStats.up.sql
+++ migrations/000048_HealthAuthorityStats.up.sql
@@ -29,40 +29,10 @@ CREATE TABLE HealthAuthorityStats(
     publish INT NOT NULL DEFAULT 0,
     teks INT NOT NULL DEFAULT 0,
     revisions INT NOT NULL DEFAULT 0,
-    -- Age of the oldest TEKs from an individual publish request.
-    oldest_tek_0_days INT NOT NULL DEFAULT 0,
-    oldest_tek_1_days INT NOT NULL DEFAULT 0,
-    oldest_tek_2_days INT NOT NULL DEFAULT 0,
-    oldest_tek_3_days INT NOT NULL DEFAULT 0,
-    oldest_tek_4_days INT NOT NULL DEFAULT 0,
-    oldest_tek_5_days INT NOT NULL DEFAULT 0,
-    oldest_tek_6_days INT NOT NULL DEFAULT 0,
-    oldest_tek_7_days INT NOT NULL DEFAULT 0,
-    oldest_tek_8_days INT NOT NULL DEFAULT 0,
-    oldest_tek_9_days INT NOT NULL DEFAULT 0,
-    oldest_tek_10_days INT NOT NULL DEFAULT 0,
-    oldest_tek_11_days INT NOT NULL DEFAULT 0,
-    oldest_tek_12_days INT NOT NULL DEFAULT 0,
-    oldest_tek_13_days INT NOT NULL DEFAULT 0,
-    oldest_tek_14_days INT NOT NULL DEFAULT 0,
-    oldest_tek_gt_14_days INT NOT NULL DEFAULT 0,
-    -- Symptom onset to upload ranges
-    onset_age_0_days INT NOT NULL DEFAULT 0,
-    onset_age_1_days INT NOT NULL DEFAULT 0,
-    onset_age_2_days INT NOT NULL DEFAULT 0,
-    onset_age_3_days INT NOT NULL DEFAULT 0,
-    onset_age_4_days INT NOT NULL DEFAULT 0,
-    onset_age_5_days INT NOT NULL DEFAULT 0,
-    onset_age_6_days INT NOT NULL DEFAULT 0,
-    onset_age_7_days INT NOT NULL DEFAULT 0,
-    onset_age_8_days INT NOT NULL DEFAULT 0,
-    onset_age_9_days INT NOT NULL DEFAULT 0,
-    onset_age_10_days INT NOT NULL DEFAULT 0,
-    onset_age_11_days INT NOT NULL DEFAULT 0,
-    onset_age_12_days INT NOT NULL DEFAULT 0,
-    onset_age_13_days INT NOT NULL DEFAULT 0,
-    onset_age_14_days INT NOT NULL DEFAULT 0,
-    onset_age_gt_14_days INT NOT NULL DEFAULT 0
+    -- Age of the oldest TEKs from an individual publish request. Index is number of days.
+    oltest_tek_days INT [],    
+    -- Symptom onset to upload ranges, index is number of days.
+    onset_age_days INT []
 );
 
 CREATE UNIQUE INDEX
