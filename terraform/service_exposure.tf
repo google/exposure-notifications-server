8c2492fafe2cd452056bcfc83ba176b12749a0d9
Yuchen Ying <ych@google.com>
2020-12-15 12:14:42 -0800

    Add a knob to enable Load Balancer logging.

--- terraform/service_exposure.tf
+++ terraform/service_exposure.tf
@@ -171,6 +171,9 @@ resource "google_compute_backend_service" "exposure" {
     group = google_compute_region_network_endpoint_group.exposure[0].id
   }
   security_policy = google_compute_security_policy.cloud-armor.name
+  log_config = {
+    enable = var.enable-lb-logging
+  }
 }
 
 output "exposure_urls" {
