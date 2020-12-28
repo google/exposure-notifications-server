086af19c443614aeabf096aa2a9e86467364007f
Seth Vargo <seth@sethvargo.com>
2020-12-28 10:59:34 -0500

    Add zapw and bodyclose (#1254)

--- tools/tools.go
+++ tools/tools.go
@@ -19,6 +19,8 @@ package tools
 
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/sethvargo/zapw/cmd/zapw"
+	_ "github.com/timakin/bodyclose"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
