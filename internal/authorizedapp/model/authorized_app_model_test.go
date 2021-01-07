28044f6cf1d3f87672e2dacf4c207d266d8b4f52
Mike Helmick <helmick@google.com>
2021-01-06 18:15:29 -0800

    improve test coverage on authorized app database/model (#1264)

--- internal/authorizedapp/model/authorized_app_model_test.go
+++ internal/authorizedapp/model/authorized_app_model_test.go
@@ -15,7 +15,11 @@
 package model
 
 import (
+	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestAuthorizedApp_IsAllowedRegion(t *testing.T) {
@@ -30,5 +34,87 @@ func TestAuthorizedApp_IsAllowedRegion(t *testing.T) {
 
 	if ok := cfg.IsAllowedRegion("CA"); ok {
 		t.Errorf("expected CA to not be allowed")
+	}
+}
+
+func TestAllAllowedRegions(t *testing.T) {
+	cfg := NewAuthorizedApp()
+	cfg.AllowedRegions = map[string]struct{}{
+		"US": {},
+		"CA": {},
+	}
+
+	got := cfg.AllAllowedRegions()
+	want := []string{"US", "CA"}
+	sorter := cmpopts.SortSlices(func(a, b string) bool {
+		return strings.Compare(a, b) <= 0
+	})
+	if diff := cmp.Diff(want, got, sorter); diff != "" {
+		t.Fatalf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestRegionsOnePerLine(t *testing.T) {
+	cfg := NewAuthorizedApp()
+	cfg.AllowedRegions = map[string]struct{}{
+		"US": {},
+		"CA": {},
+	}
+
+	got := cfg.RegionsOnePerLine()
+	want := "CA\nUS"
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestAllAllowedHealthAuthorityIDs(t *testing.T) {
+	cfg := NewAuthorizedApp()
+	cfg.AllowedHealthAuthorityIDs[12] = struct{}{}
+	cfg.AllowedHealthAuthorityIDs[42] = struct{}{}
+
+	got := cfg.AllAllowedHealthAuthorityIDs()
+	want := []int64{12, 42}
+	sorter := cmpopts.SortSlices(func(a, b int64) bool {
+		return a <= b
+	})
+	if diff := cmp.Diff(want, got, sorter); diff != "" {
+		t.Fatalf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestValidate(t *testing.T) {
+	cfg := NewAuthorizedApp()
+
+	got := cfg.Validate()
+	want := []string{
+		"Health Authority ID cannot be empty",
+		"Regions list cannot be empty",
+	}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("mismatch (-want, +got):\n%s", diff)
+	}
+}
+
+func TestIsAllowedRegions(t *testing.T) {
+	cfg := NewAuthorizedApp()
+
+	if !cfg.IsAllowedRegion("foo") {
+		t.Errorf("region disallowed when all regions should be allowed")
+	}
+
+	cfg.AllowedRegions = map[string]struct{}{
+		"US": {},
+		"CA": {},
+	}
+
+	if !cfg.IsAllowedRegion("US") {
+		t.Errorf("missing expected allowed region: US")
+	}
+	if !cfg.IsAllowedRegion("CA") {
+		t.Errorf("missing expected allowed region: CA")
+	}
+	if cfg.IsAllowedRegion("GB") {
+		t.Errorf("unexpected allowed region: GB")
 	}
 }
