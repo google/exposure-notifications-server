13d206b7388d784d0c100663e9380bf4664c7d24
Mike Helmick <helmick@google.com>
2021-02-10 10:30:13 -0800

    fix incomplete license (#1369)

--- internal/e2e/e2e_test.go
+++ internal/e2e/e2e_test.go
@@ -1,5 +1,3 @@
-// +build e2e
-
 // Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +9,10 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific la
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build e2e
 
 package e2etest
 
