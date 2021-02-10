c24ddf25f1d9aa2170cf524e9679b7a38129847b
Yuchen Ying <github.com@yegle.net>
2021-02-10 09:32:57 -0800

    Add common observability code (#1364)
    * Update WithBuildInfo() to be more generic.
    
    We can then share this method to be used by verification server.
    
    * Move common observability code into key server repo.
    
    * Revert go.{mod,sum} changes.
    
    * Remove unused variables.
    
    * Remove common.go and move everything to observability.go

--- pkg/observability/observability.go
+++ pkg/observability/observability.go
@@ -22,10 +22,12 @@ import (
 	"io"
 	"os"
 	"sync"
+	"time"
 
 	"contrib.go.opencensus.io/integrations/ocsql"
 	"go.opencensus.io/plugin/ocgrpc"
 	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
 )
@@ -33,6 +35,59 @@ import (
 // OCSQLDriverName is the name of the SQL driver wrapped by OpenCensus
 // instrumentation code.
 const OCSQLDriverName = "ocsql"
+
+var (
+	// BlameTagKey indicating Who to blame for the API request failure.
+	// NONE: no failure
+	// CLIENT: the client is at fault (e.g. invalid request)
+	// SERVER: the server is at fault
+	// EXTERNAL: some third party is at fault
+	// UNKNOWN: for everything else
+	BlameTagKey = tag.MustNewKey("blame")
+
+	// ResultTagKey contains a free format text describing the result of the
+	// request. Preferably ALL CAPS WITH UNDERSCORE.
+	// OK indicating a successful request.
+	// You can losely base this string on
+	// https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
+	// but feel free to use any text as long as it's easy to filter.
+	ResultTagKey = tag.MustNewKey("result")
+)
+
+var (
+	// BlameNone indicate no API failure
+	BlameNone = tag.Upsert(BlameTagKey, "NONE")
+
+	// BlameClient indicate the client is at fault (e.g. invalid request)
+	BlameClient = tag.Upsert(BlameTagKey, "CLIENT")
+
+	// BlameServer indicate the server is at fault
+	BlameServer = tag.Upsert(BlameTagKey, "SERVER")
+
+	// BlameExternal indicate some third party is at fault
+	BlameExternal = tag.Upsert(BlameTagKey, "EXTERNAL")
+
+	// BlameUnknown can be used for everything else
+	BlameUnknown = tag.Upsert(BlameTagKey, "UNKNOWN")
+)
+
+var (
+	// ResultOK add a tag indicating the API call is a success.
+	ResultOK = tag.Upsert(ResultTagKey, "OK")
+	// ResultNotOK add a tag indicating the API call is a failure.
+	ResultNotOK = ResultError("NOT_OK")
+)
+
+// ResultError add a tag with the given string as the result.
+func ResultError(result string) tag.Mutator {
+	return tag.Upsert(ResultTagKey, result)
+}
+
+// BuildInfo is the interface to provide build information.
+type BuildInfo interface {
+	ID() string
+	Tag() string
+}
 
 var (
 	BuildIDTagKey  = tag.MustNewKey("build_id")
@@ -138,4 +193,21 @@ func WithBuildInfo(ctx context.Context, info BuildInfo) context.Context {
 
 	ctx, _ = tag.New(ctx, tags...)
 	return ctx
+}
+
+// RecordLatency calculate and record the latency.
+// Usage example:
+// func foo() {
+// 	 defer RecordLatency(&ctx, time.Now(), metric, tag1, tag2)
+//   // remaining of the function body.
+// }
+func RecordLatency(ctx context.Context, start time.Time, m *stats.Float64Measure, mutators ...*tag.Mutator) {
+	var additionalMutators []tag.Mutator
+	for _, t := range mutators {
+		additionalMutators = append(additionalMutators, *t)
+	}
+	// Calculate the millisecond number as float64. time.Duration.Millisecond()
+	// returns an integer.
+	latency := float64(time.Since(start)) / float64(time.Millisecond)
+	stats.RecordWithTags(ctx, additionalMutators, m.M(latency))
 }
