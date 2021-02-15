3abe429f19fb3c4ff1e5896c2dabd579c00707f0
Seth Vargo <seth@sethvargo.com>
2021-02-15 11:21:58 -0500

    Ignore more annotations in Terraform diff (#1379)

--- terraform/service_export_importer.tf
+++ terraform/service_export_importer.tf
@@ -127,10 +127,14 @@ resource "google_cloud_run_service" "export-importer" {
       metadata[0].annotations["run.googleapis.com/client-name"],
       metadata[0].annotations["run.googleapis.com/client-version"],
       metadata[0].annotations["run.googleapis.com/ingress-status"],
+      metadata[0].annotations["serving.knative.dev/creator"],
+      metadata[0].annotations["serving.knative.dev/lastModifier"],
       metadata[0].labels["cloud.googleapis.com/location"],
       template[0].metadata[0].annotations["client.knative.dev/user-image"],
       template[0].metadata[0].annotations["run.googleapis.com/client-name"],
       template[0].metadata[0].annotations["run.googleapis.com/client-version"],
+      template[0].metadata[0].annotations["serving.knative.dev/creator"],
+      template[0].metadata[0].annotations["serving.knative.dev/lastModifier"],
       template[0].spec[0].containers[0].image,
     ]
   }
