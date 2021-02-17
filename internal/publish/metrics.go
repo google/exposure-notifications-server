google/exposure-notifications-server

commit 6fffb74aed09c557f2e03d6f8659b6ff5cebcde0

Remove the reference to the metrics middleware. (#1226)
* Remove the reference to the metrics middleware.

Per the internal discussion, we don't actually use the log based metric
exporter. It would be simpler to just call stats.Record directly without
an extra layer of indirection.

* fixup! Remove the reference to the metrics middleware.

* fixup! fixup! Remove the reference to the metrics middleware.

internal/publish/publish.go
@@ -25,10 +25,11 @@ import (
 	"net/http"
 	"time"
 
+	"go.opencensus.io/stats"
 	"go.opencensus.io/trace"
 
 	"github.com/google/exposure-notifications-server/internal/authorizedapp"
-	"github.com/google/exposure-notifications-server/internal/metrics/metricsware"
+	"github.com/google/exposure-notifications-server/internal/metrics/publish"
 	"github.com/google/exposure-notifications-server/internal/pb"
 	"github.com/google/exposure-notifications-server/internal/publish/database"
 	"github.com/google/exposure-notifications-server/internal/publish/model"
@@ -189,8 +190,6 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 	defer span.End()
 
 	logger := logging.FromContext(ctx).With("health_authority_id", data.HealthAuthorityID)
-	metrics := h.serverenv.MetricsExporter(ctx)
-	metricsMiddleWare := metricsware.NewMiddleWare(&metrics)
 
 	logger.Info("publish API request")
 
@@ -209,7 +208,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 					Code:         verifyapi.ErrorUnknownHealthAuthorityID,
 				},
 				metrics: func() {
-					metricsMiddleWare.RecordHealthAuthorityNotAuthorized(ctx)
+					stats.Record(ctx, publish.HealthAuthorityNotAuthorized.M(1))
 				},
 			}
 		}
@@ -228,7 +227,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 				Code:         verifyapi.ErrorUnableToLoadHealthAuthority,
 			},
 			metrics: func() {
-				metricsMiddleWare.RecordErrorLoadingAuthorizedApp(ctx)
+				stats.Record(ctx, publish.ErrorLoadingAuthorizedApp.M(1))
 			},
 		}
 	}
@@ -250,7 +249,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 						ErrorMessage: message, // Error code omitted, since this isn't in the v1 path.
 					},
 					metrics: func() {
-						metricsMiddleWare.RecordRegionNotAuthorized(ctx)
+						stats.Record(ctx, publish.RegionNotAuthorized.M(1))
 					},
 				}
 			}
@@ -280,7 +279,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 				Code:         verifyapi.ErrorHealthAuthorityMissingRegionConfiguration,
 			},
 			metrics: func() {
-				metricsMiddleWare.RecordRegionNotSpecified(ctx)
+				stats.Record(ctx, publish.RegionNotSpecified.M(1))
 			},
 		}
 	}
@@ -290,7 +289,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 	if err != nil {
 		if appConfig.BypassHealthAuthorityVerification {
 			logger.Warnf("bypassing health authority certificate verification health authority: %v", appConfig.AppPackageName)
-			metricsMiddleWare.RecordVerificationBypassed(ctx)
+			stats.Record(ctx, publish.VerificationBypassed.M(1))
 		} else {
 			message := fmt.Sprintf("unable to validate diagnosis verification: %v", err)
 			if h.config.DebugLogBadCertificates {
@@ -306,7 +305,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 					Code:         verifyapi.ErrorVerificationCertificateInvalid,
 				},
 				metrics: func() {
-					metricsMiddleWare.RecordBadVerification(ctx)
+					stats.Record(ctx, publish.BadVerification.M(1))
 				},
 			}
 		}
@@ -346,7 +345,7 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 				Warnings:     transformWarnings,
 			},
 			metrics: func() {
-				metricsMiddleWare.RecordTransformFail(ctx)
+				stats.Record(ctx, publish.TransformFail.M(1))
 			},
 		}
 	}
@@ -396,7 +395,10 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 				Warnings:     transformWarnings,
 			},
 			metrics: func() {
-				metricsMiddleWare.RecordRevisionTokenIssue(ctx, metric)
+				// TODO(yegle): we want to use this string later when we
+				// refactor the metrics.
+				_ = metric
+				stats.Record(ctx, publish.RevisionTokenIssue.M(1))
 			},
 		}
 	}
@@ -443,9 +445,9 @@ func (h *PublishHandler) process(ctx context.Context, data *verifyapi.Publish, b
 		status:      http.StatusOK,
 		pubResponse: &publishResponse,
 		metrics: func() {
-			metricsMiddleWare.RecordInsertions(ctx, int(resp.Inserted))
-			metricsMiddleWare.RecordRevisions(ctx, int(resp.Revised))
-			metricsMiddleWare.RecordDrops(ctx, int(resp.Dropped))
+			stats.Record(ctx, publish.ExposuresInserted.M(int64(resp.Inserted)))
+			stats.Record(ctx, publish.ExposuresInserted.M(int64(resp.Revised)))
+			stats.Record(ctx, publish.ExposuresInserted.M(int64(resp.Dropped)))
 		},
 	}
 }

internal/publish/publish_v1.go
@@ -20,11 +20,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"go.opencensus.io/stats"
 	"go.opencensus.io/trace"
 
 	"github.com/google/exposure-notifications-server/internal/jsonutil"
 	"github.com/google/exposure-notifications-server/internal/maintenance"
-	"github.com/google/exposure-notifications-server/internal/metrics/metricsware"
+	"github.com/google/exposure-notifications-server/internal/metrics/publish"
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
 	"github.com/google/exposure-notifications-server/pkg/logging"
 	"github.com/mikehelmick/go-chaff"
@@ -33,9 +34,6 @@ import (
 func (h *PublishHandler) handleRequest(w http.ResponseWriter, r *http.Request) *response {
 	ctx, span := trace.StartSpan(r.Context(), "(*publish.PublishHandler).handleRequest")
 	defer span.End()
-
-	metrics := h.serverenv.MetricsExporter(ctx)
-	metricsMiddleware := metricsware.NewMiddleWare(&metrics)
 
 	w.Header().Set(HeaderAPIVersion, "v1")
 
@@ -55,7 +53,7 @@ func (h *PublishHandler) handleRequest(w http.ResponseWriter, r *http.Request) *
 				Code:         errorCode,
 			},
 			metrics: func() {
-				metricsMiddleware.RecordBadJSON(ctx)
+				stats.Record(ctx, publish.BadJSON.M(1))
 			},
 		}
 	}
@@ -72,11 +70,9 @@ func (h *PublishHandler) Handle() http.Handler {
 				response := h.handleRequest(w, r)
 
 				ctx := r.Context()
-				metrics := h.serverenv.MetricsExporter(ctx)
-				metricsMiddleware := metricsware.NewMiddleWare(&metrics)
 
 				if err := response.padResponse(h.config); err != nil {
-					metricsMiddleware.RecordPaddingFailure(ctx)
+					stats.Record(ctx, publish.PaddingFailed.M(1))
 					logging.FromContext(ctx).Errorw("failed to pad response", "error", err)
 				}
 

internal/publish/publish_v1alpha1.go
@@ -20,11 +20,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"go.opencensus.io/stats"
 	"go.opencensus.io/trace"
 
 	"github.com/google/exposure-notifications-server/internal/jsonutil"
 	"github.com/google/exposure-notifications-server/internal/maintenance"
-	"github.com/google/exposure-notifications-server/internal/metrics/metricsware"
+	"github.com/google/exposure-notifications-server/internal/metrics/publish"
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
 	"github.com/google/exposure-notifications-server/pkg/api/v1alpha1"
 	"github.com/google/exposure-notifications-server/pkg/logging"
@@ -34,9 +35,6 @@ import (
 func (h *PublishHandler) handleV1Apha1Request(w http.ResponseWriter, r *http.Request) *response {
 	ctx, span := trace.StartSpan(r.Context(), "(*publish.PublishHandler).handleRequest")
 	defer span.End()
-
-	metrics := h.serverenv.MetricsExporter(ctx)
-	metricsMiddleware := metricsware.NewMiddleWare(&metrics)
 
 	w.Header().Set(HeaderAPIVersion, "v1alpha")
 
@@ -49,7 +47,7 @@ func (h *PublishHandler) handleV1Apha1Request(w http.ResponseWriter, r *http.Req
 			status:      code,
 			pubResponse: &verifyapi.PublishResponse{ErrorMessage: message}, // will be down-converted in ServeHTTP
 			metrics: func() {
-				metricsMiddleware.RecordBadJSON(ctx)
+				stats.Record(ctx, publish.BadJSON.M(1))
 			},
 		}
 	}
@@ -89,11 +87,9 @@ func (h *PublishHandler) HandleV1Alpha1() http.Handler {
 				response := h.handleV1Apha1Request(w, r)
 
 				ctx := r.Context()
-				metrics := h.serverenv.MetricsExporter(ctx)
-				metricsMiddleware := metricsware.NewMiddleWare(&metrics)
 
 				if err := response.padResponse(h.config); err != nil {
-					metricsMiddleware.RecordPaddingFailure(ctx)
+					stats.Record(ctx, publish.PaddingFailed.M(1))
 					logging.FromContext(ctx).Errorw("failed to padd response", "error", err)
 				}
 

commit e6ce891e417c0384b2b7aa658f78becc5c47a4aa

Clean up metrics in the Publish API (#1388)
* Add a new metric to replace the old publish API related metrics.

* Replace existing metrics with a single new metric.

* Fix metrics.

The error was introduced in 6fffb74aed09c557f2e03d6f8659b6ff5cebcde0.

* Cleanup metrics in the publish API.

* Fix typo.

Fix errcheck complaints from golangci-lint.

* Update internal/publish/publish.go

Co-authored-by: Seth Vargo <seth@sethvargo.com>

* Remove unused metrics.

Co-authored-by: Seth Vargo <seth@sethvargo.com>

internal/publish/metrics.go
@@ -18,116 +18,77 @@ package publish
 import (
 	"github.com/google/exposure-notifications-server/internal/metrics"
 	"github.com/google/exposure-notifications-server/pkg/observability"
+	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 )
 
 var (
 	publishMetricsPrefix = metrics.MetricRoot + "publish/"
 
-	mHealthAuthorityNotAuthorized = stats.Int64(publishMetricsPrefix+"pha_not_authorized",
-		"Public health authority authorized failures", stats.UnitDimensionless)
-	mErrorLoadingAuthorizedApp = stats.Int64(publishMetricsPrefix+"error_loading_app",
-		"Errors in loading authorized app", stats.UnitDimensionless)
-	mRegionNotAuthorized = stats.Int64(publishMetricsPrefix+"region_not_authorized",
-		"Instances of region not being authorized", stats.UnitDimensionless)
-	mRegionNotSpecified = stats.Int64(publishMetricsPrefix+"region_not_specified",
-		"Instances of region not being specified", stats.UnitDimensionless)
+	mLatencyMs = stats.Float64(publishMetricsPrefix+"requests", "publish requests", stats.UnitMilliseconds)
+
+	mExposuresCount = stats.Int64(publishMetricsPrefix+"exposures_count",
+		"exposure count", stats.UnitDimensionless)
+
 	mVerificationBypassed = stats.Int64(publishMetricsPrefix+"verification_bypassed",
 		"Instances of health authority verification being bypassed", stats.UnitDimensionless)
-	mBadVerification = stats.Int64(publishMetricsPrefix+"bad_verification",
-		"Instances of bad health authority verification", stats.UnitDimensionless)
-	mTransformFail = stats.Int64(publishMetricsPrefix+"transform_fail",
-		"Instances of failure fo transformation to exposure entities", stats.UnitDimensionless)
-	mRevisionTokenIssue = stats.Int64(publishMetricsPrefix+"revision_token_issue",
-		"Instances of issues in revision token processing", stats.UnitDimensionless)
-	mExposuresInserted = stats.Int64(publishMetricsPrefix+"exposures_insertions",
-		"Instances of exposure being inserted", stats.UnitDimensionless)
-	mExposuresRevised = stats.Int64(publishMetricsPrefix+"exposures_revisions",
-		"Instances of exposure being revised", stats.UnitDimensionless)
-	mExposuresDropped = stats.Int64(publishMetricsPrefix+"exposures_drops",
-		"Instances of exposure being dropped", stats.UnitDimensionless)
-	// v1 and v1alpha1
-	mBadJSON = stats.Int64(publishMetricsPrefix+"bad_json",
-		"Instances of bad JSON in incoming request", stats.UnitDimensionless)
 	// v1 and v1alpha1
 	mPaddingFailed = stats.Int64(publishMetricsPrefix+"padding_failed",
 		"Instances of response padding failures", stats.UnitDimensionless)
+
+	exposureTypeTag = tag.MustNewKey("type")
+
+	requestTagKeys = []tag.Key{
+		observability.BuildIDTagKey,
+		observability.BuildTagTagKey,
+		observability.BlameTagKey,
+		observability.ResultTagKey,
+	}
+	exposureTagKeys = []tag.Key{
+		observability.BuildIDTagKey,
+		observability.BuildTagTagKey,
+		exposureTypeTag,
+	}
+)
+
+func exposureType(s string) tag.Mutator {
+	return tag.Upsert(exposureTypeTag, s)
+}
+
+var (
+	exposuresInserted = exposureType("INSERTED")
+	exposuresRevised  = exposureType("REVISED")
+	exposuresDropped  = exposureType("DROPPED")
 )
 
 func init() {
 	observability.CollectViews([]*view.View{
 		{
-			Name:        metrics.MetricRoot + "pha_not_authorized_count",
-			Description: "Total count of authorization failures for the Public Health Authority",
-			Measure:     mHealthAuthorityNotAuthorized,
-			Aggregation: view.Sum(),
+			Name:        metrics.MetricRoot + "requests_count",
+			Description: "Total count of publish requests",
+			Measure:     mLatencyMs,
+			Aggregation: view.Count(),
+			TagKeys:     requestTagKeys,
 		},
 		{
-			Name:        metrics.MetricRoot + "error_loading_app_count",
-			Description: "Total count of errors in loading the authorized application",
-			Measure:     mErrorLoadingAuthorizedApp,
-			Aggregation: view.Sum(),
+			Name:        metrics.MetricRoot + "requests_latency",
+			Description: "Latency distribution of publish requests",
+			Measure:     mLatencyMs,
+			Aggregation: ochttp.DefaultLatencyDistribution,
+			TagKeys:     requestTagKeys,
 		},
 		{
-			Name:        metrics.MetricRoot + "region_not_authorized_count",
-			Description: "Total count of region not being authorized",
-			Measure:     mRegionNotAuthorized,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "region_not_specified_count",
-			Description: "Total count of region not being specified",
-			Measure:     mRegionNotSpecified,
-			Aggregation: view.Sum(),
+			Name:        metrics.MetricRoot + "exposures_count",
+			Description: "Total count of published exposures.",
+			Measure:     mExposuresCount,
+			TagKeys:     exposureTagKeys,
 		},
 		{
 			Name:        metrics.MetricRoot + "verification_bypassed_count",
 			Description: "Total count of health authority verification being bypassed",
 			Measure:     mVerificationBypassed,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "bad_verification_count",
-			Description: "Total count of bad health authority verification",
-			Measure:     mBadVerification,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "transform_fail_count",
-			Description: "Total count of transformations failures of exposure entities",
-			Measure:     mTransformFail,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "revision_token_issue_count",
-			Description: "Total count of revision token processing issues",
-			Measure:     mRevisionTokenIssue,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "exposures_insertions_count",
-			Description: "Total count of exposure insertions",
-			Measure:     mExposuresInserted,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "exposures_revisions_count",
-			Description: "Total count of exposure revisions",
-			Measure:     mExposuresRevised,
-			Aggregation: view.Sum(),
-		},
-		{
-			Name:        metrics.MetricRoot + "exposures_drops_count",
-			Description: "Total count of exposure drops",
-			Measure:     mExposuresDropped,
-			Aggregation: view.Sum(),
-		},
-		// v1 and v1alpha1
-		{
-			Name:        metrics.MetricRoot + "bad_json",
-			Description: "Total count of bad JSON in incoming requests",
-			Measure:     mBadJSON,
 			Aggregation: view.Sum(),
 		},
 		// v1 and v1alpha1

internal/publish/publish.go
@@ -26,6 +26,7 @@ import (
 	"time"
 
 	"go.opencensus.io/stats"
+	"go.opencensus.io/tag"
 	"go.opencensus.io/trace"
 
 	"github.com/google/exposure-notifications-server/internal/authorizedapp"
@@ -41,6 +42,7 @@ import (
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
 	"github.com/google/exposure-notifications-server/pkg/base64util"
 	"github.com/google/exposure-notifications-server/pkg/logging"
+	obs "github.com/google/exposure-notifications-server/pkg/observability"
 	"github.com/google/exposure-notifications-server/pkg/server"
 	"github.com/gorilla/mux"
 	"github.com/mikehelmick/go-chaff"
@@ -162,9 +164,6 @@ func (s *Server) Routes(ctx context.Context) *mux.Router {
 type response struct {
 	status      int
 	pubResponse *verifyapi.PublishResponse
-
-	// metrics is a function to execute to publish metrics
-	metrics func()
 }
 
 func generatePadding(minPadding, paddingRange int64) (string, error) {
@@ -214,6 +213,10 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 	ctx, span := trace.StartSpan(ctx, "(*publish.PublishHandler).process")
 	defer span.End()
 
+	blame := obs.BlameNone
+	obsResult := obs.ResultOK
+	defer obs.RecordLatency(ctx, time.Now(), mLatencyMs, &blame, &obsResult)
+
 	logger := logging.FromContext(ctx).Named("process").
 		With("health_authority_id", data.HealthAuthorityID)
 
@@ -227,14 +230,13 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		if errors.Is(err, authorizedapp.ErrAppNotFound) {
 			message := fmt.Sprintf("unauthorized health authority: %v", data.HealthAuthorityID)
 			span.SetStatus(trace.Status{Code: trace.StatusCodeInternal, Message: message})
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("ERROR_UNAUTHORIZED_HEALTH_AUTHORITY")
 			return &response{
 				status: http.StatusUnauthorized,
 				pubResponse: &verifyapi.PublishResponse{
 					ErrorMessage: message,
 					Code:         verifyapi.ErrorUnknownHealthAuthorityID,
-				},
-				metrics: func() {
-					stats.Record(ctx, mHealthAuthorityNotAuthorized.M(1))
 				},
 			}
 		}
@@ -246,14 +248,13 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		// and no other data from the request.
 		message := fmt.Sprintf("error loading health authority config: %v", err)
 		span.SetStatus(trace.Status{Code: trace.StatusCodeInternal, Message: message})
+		blame = obs.BlameServer
+		obsResult = obs.ResultError("ERROR_LOADING_HEALTH_AUTHORITY")
 		return &response{
 			status: http.StatusNotFound,
 			pubResponse: &verifyapi.PublishResponse{
 				ErrorMessage: message,
 				Code:         verifyapi.ErrorUnableToLoadHealthAuthority,
-			},
-			metrics: func() {
-				stats.Record(ctx, mErrorLoadingAuthorizedApp.M(1))
 			},
 		}
 	}
@@ -269,13 +270,12 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 				err := fmt.Errorf("app %v tried to write to unauthorized region %v", appConfig.AppPackageName, r)
 				message := fmt.Sprintf("verifying allowed regions: %v", err)
 				span.SetStatus(trace.Status{Code: trace.StatusCodePermissionDenied, Message: message})
+				blame = obs.BlameClient
+				obsResult = obs.ResultError("ERROR_REGION_NOT_AUTHORIZED")
 				return &response{
 					status: http.StatusUnauthorized,
 					pubResponse: &verifyapi.PublishResponse{
 						ErrorMessage: message, // Error code omitted, since this isn't in the v1 path.
-					},
-					metrics: func() {
-						stats.Record(ctx, mRegionNotAuthorized.M(1))
 					},
 				}
 			}
@@ -298,14 +298,13 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		logger.Errorf("No regions present in request or configured for HealthAuthorityID: %v", data.HealthAuthorityID)
 		message := fmt.Sprintf("unknown health authority regions for %v", data.HealthAuthorityID)
 		span.SetStatus(trace.Status{Code: trace.StatusCodePermissionDenied, Message: message})
+		blame = obs.BlameClient
+		obsResult = obs.ResultError("ERROR_REGION_NOT_SPECIFIED")
 		return &response{
 			status: http.StatusInternalServerError,
 			pubResponse: &verifyapi.PublishResponse{
 				ErrorMessage: message,
 				Code:         verifyapi.ErrorHealthAuthorityMissingRegionConfiguration,
-			},
-			metrics: func() {
-				stats.Record(ctx, mRegionNotSpecified.M(1))
 			},
 		}
 	}
@@ -324,14 +323,13 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 				logger.Errorw(message, "error", err)
 			}
 			span.SetStatus(trace.Status{Code: trace.StatusCodeInvalidArgument, Message: message})
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("BAD_VERIFICATION")
 			return &response{
 				status: http.StatusUnauthorized,
 				pubResponse: &verifyapi.PublishResponse{
 					ErrorMessage: message,
 					Code:         verifyapi.ErrorVerificationCertificateInvalid,
-				},
-				metrics: func() {
-					stats.Record(ctx, mBadVerification.M(1))
 				},
 			}
 		}
@@ -367,15 +365,14 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		message := fmt.Sprintf("unable to read request data: %v", transformError)
 		logger.Error(message)
 		span.SetStatus(trace.Status{Code: trace.StatusCodeInvalidArgument, Message: message})
+		blame = obs.BlameClient
+		obsResult = obs.ResultError("TRANSFORM_FAILED")
 		return &response{
 			status: http.StatusBadRequest,
 			pubResponse: &verifyapi.PublishResponse{
 				ErrorMessage: message,
 				Code:         verifyapi.ErrorBadRequest,
 				Warnings:     transformWarnings,
-			},
-			metrics: func() {
-				stats.Record(ctx, mTransformFail.M(1))
 			},
 		}
 	}
@@ -397,30 +394,38 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		status := http.StatusBadRequest
 		var logMessage, errorMessage, errorCode string
 		var errInvalidReportTypeTransition *model.ErrorKeyInvalidReportTypeTransition
-		metric := "publish-revision-token-issue"
 		switch {
 		case decryptFail || errors.Is(err, database.ErrExistingKeyNotInToken) || errors.Is(err, database.ErrRevisionTokenMetadataMismatch):
 			logMessage = fmt.Sprintf("revision token present, but invalid: %v", err)
 			errorMessage = "revision token is invalid"
 			errorCode = verifyapi.ErrorInvalidRevisionToken
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("INVALID_REVISION_TOKEN")
 		case errors.Is(err, database.ErrNoRevisionToken):
 			logMessage = "no revision token"
 			errorMessage = "no revision token, but sent existing keys"
 			errorCode = verifyapi.ErrorMissingRevisionToken
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("MISSING_REVISION_TOKEN")
 		case errors.Is(err, model.ErrorKeyAlreadyRevised):
 			logMessage = "key already revised"
 			errorMessage = "key was already revised"
 			errorCode = verifyapi.ErrorKeyAlreadyRevised
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("KEY_ALREADY_REVISED")
 		case errors.As(err, &errInvalidReportTypeTransition):
 			logMessage = errInvalidReportTypeTransition.Error()
 			errorMessage = errInvalidReportTypeTransition.Error()
 			errorCode = verifyapi.ErrorInvalidReportTypeTransition
+			blame = obs.BlameClient
+			obsResult = obs.ResultError("INVALID_REPORT_TYPE_TRANSITION")
 		default:
 			logMessage = fmt.Sprintf("error writing exposure record: %v", err)
 			errorMessage = http.StatusText(http.StatusInternalServerError)
 			errorCode = verifyapi.ErrorInternalError
-			metric = "publish-db-write-error"
 			logger.Errorw("publish error", "error", logMessage)
+			blame = obs.BlameServer
+			obsResult = obs.ResultError("ERROR_DB_WRITE")
 		}
 		logger.Debugw("publish error", "error", logMessage)
 		span.SetStatus(trace.Status{Code: trace.StatusCodeInternal, Message: logMessage})
@@ -430,12 +435,6 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 				ErrorMessage: errorMessage,
 				Code:         errorCode,
 				Warnings:     transformWarnings,
-			},
-			metrics: func() {
-				// TODO(yegle): we want to use this string later when we
-				// refactor the metrics.
-				_ = metric
-				stats.Record(ctx, mRevisionTokenIssue.M(1))
 			},
 		}
 	}
@@ -478,14 +477,20 @@ func (s *Server) process(ctx context.Context, data *verifyapi.Publish, platform 
 		publishResponse.ErrorMessage = transformError.Error()
 	}
 
+	exposureCounts := map[tag.Mutator]uint32{
+		exposuresInserted: resp.Inserted,
+		exposuresRevised:  resp.Revised,
+		exposuresDropped:  resp.Dropped,
+	}
+	for t, n := range exposureCounts {
+		if err := stats.RecordWithTags(ctx, []tag.Mutator{t}, mExposuresCount.M(int64(n))); err != nil {
+			logger.Errorw("failed to record stats", "error", err)
+		}
+	}
+
 	return &response{
 		status:      http.StatusOK,
 		pubResponse: &publishResponse,
-		metrics: func() {
-			stats.Record(ctx, mExposuresInserted.M(int64(resp.Inserted)))
-			stats.Record(ctx, mExposuresRevised.M(int64(resp.Revised)))
-			stats.Record(ctx, mExposuresDropped.M(int64(resp.Dropped)))
-		},
 	}
 }
 

internal/publish/publish_v1.go
@@ -18,6 +18,7 @@ package publish
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"go.opencensus.io/stats"
 	"go.opencensus.io/trace"
@@ -25,6 +26,7 @@ import (
 	"github.com/google/exposure-notifications-server/internal/jsonutil"
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
 	"github.com/google/exposure-notifications-server/pkg/logging"
+	obs "github.com/google/exposure-notifications-server/pkg/observability"
 )
 
 func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) *response {
@@ -36,6 +38,9 @@ func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) *response
 	var data verifyapi.Publish
 	code, err := jsonutil.Unmarshal(w, r, &data)
 	if err != nil {
+		blame := obs.BlameClient
+		obsResult := obs.ResultError("BAD_JSON")
+		defer obs.RecordLatency(ctx, time.Now(), mLatencyMs, &blame, &obsResult)
 		message := fmt.Sprintf("error unmarshalling API call, code: %v: %v", code, err)
 		span.SetStatus(trace.Status{Code: trace.StatusCodeInternal, Message: message})
 		errorCode := verifyapi.ErrorBadRequest
@@ -47,9 +52,6 @@ func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) *response
 			pubResponse: &verifyapi.PublishResponse{
 				ErrorMessage: message,
 				Code:         errorCode,
-			},
-			metrics: func() {
-				stats.Record(ctx, mBadJSON.M(1))
 			},
 		}
 	}
@@ -72,10 +74,6 @@ func (s *Server) handlePublishV1() http.Handler {
 			logger.Errorw("failed to pad response", "error", err)
 		} else {
 			response.pubResponse.Padding = padding
-		}
-
-		if response.metrics != nil {
-			response.metrics()
 		}
 
 		jsonutil.MarshalResponse(w, response.status, response.pubResponse)

internal/publish/publish_v1alpha1.go
@@ -18,6 +18,7 @@ package publish
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"go.opencensus.io/stats"
 	"go.opencensus.io/trace"
@@ -26,6 +27,7 @@ import (
 	verifyapi "github.com/google/exposure-notifications-server/pkg/api/v1"
 	"github.com/google/exposure-notifications-server/pkg/api/v1alpha1"
 	"github.com/google/exposure-notifications-server/pkg/logging"
+	obs "github.com/google/exposure-notifications-server/pkg/observability"
 )
 
 func (s *Server) handleV1Apha1Request(w http.ResponseWriter, r *http.Request) *response {
@@ -39,12 +41,12 @@ func (s *Server) handleV1Apha1Request(w http.ResponseWriter, r *http.Request) *r
 	if err != nil {
 		message := fmt.Sprintf("error unmarshalling API call, code: %v: %v", code, err)
 		span.SetStatus(trace.Status{Code: trace.StatusCodeInternal, Message: message})
+		blame := obs.BlameClient
+		obsResult := obs.ResultError("BAD_JSON")
+		defer obs.RecordLatency(ctx, time.Now(), mLatencyMs, &blame, &obsResult)
 		return &response{
 			status:      code,
 			pubResponse: &verifyapi.PublishResponse{ErrorMessage: message}, // will be down-converted in ServeHTTP
-			metrics: func() {
-				stats.Record(ctx, mBadJSON.M(1))
-			},
 		}
 	}
 
@@ -98,10 +100,6 @@ func (s *Server) handlePublishV1Alpha1() http.Handler {
 			Error:             response.pubResponse.ErrorMessage,
 			Padding:           response.pubResponse.Padding,
 			Warnings:          response.pubResponse.Warnings,
-		}
-
-		if response.metrics != nil {
-			response.metrics()
 		}
 
 		jsonutil.MarshalResponse(w, response.status, alpha1Response)