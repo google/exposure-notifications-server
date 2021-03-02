66a1556f486fbfc16643f3db15004bc931ed8254
Marilia Melo <mariliamelo@gmail.com>
2021-03-02 10:08:48 -0800

    Add uptime check for known hosts (#1413)
    * Add uptime check for known hosts
    
    * Add uptime check for known hosts
    
    * move probers.tf inside alerting
    
    * add hosts to variables file
    
    * add hostdown alert
    
    * Rename Hostdown.md to HostDown.md
    
    * Address comments

---
+++ docs/playbooks/alerts/HostDown.md
@@ -0,0 +1,26 @@
+# HostDown
+
+This is an uptime check on the custom domain. Failing these checks means
+the clients cannot reach the verification server via the encv.org
+hostnames.
+
+The check happens every 60s, and this alert will fire if the server
+failed the check for 5min.
+
+## Triage steps
+
+There are multiple components involved here so you need to thoroughly
+check every one of them:
+
+- Check the IP address assigned by Cloud Load Balancer: run `terraform
+  output` and check the `lb_ip` from the result.
+- Run `dig` the host URL and ensure the IP address
+  matches the one you get above.
+- Check https://status.cloud.google.com/ and see if any related services
+  are having an outage right now.
+- If none of the above steps yield useful insight, try follow
+https://cloud.google.com/load-balancing/docs/https/troubleshooting-ext-https-lbs
+  and troubleshoot the issue assuming it's Cloud Load Balancer's
+  problem.
+- Check Cloud Run logs and see if there's any obvious error.
+- If nothing obiously wrong, open a Cloud Support ticket.
---
+++ terraform/alerting/probers.tf
@@ -0,0 +1,42 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_monitoring_uptime_check_config" "https" {
+  project = var.project
+
+  for_each = toset(compact(concat(var.debugger_hosts, var.export_hosts, var.exposure_hosts, var.federationout_hosts)))
+
+  display_name = each.key
+  timeout      = "10s"
+  period       = "60s"
+
+  http_check {
+    path         = "/health"
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.project
+      host       = each.key
+    }
+  }
+
+  depends_on = [
+    null_resource.manual-step-to-enable-workspace,
+  ]
+}
